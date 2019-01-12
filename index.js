22846c229cc0038ed58a60f541b0143b1d8cea4b
Elizabeth Chan <lchan217@gmail.com>
2019-01-12 14:39:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-function theBeatlesPlay(musician, instruments){
-  var newArray = [];
-  for(var i = 0; i<musician.length; i++) {
-    newArray.push(musician[i] + " plays " + instruments[i])
-  }
-  return newArray
+function theBeatlesPlay(musicians, instruments){
+  var emptyArray = [];
+  for(let i=0; i<musicians.length; i++){
+    emptyArray.push(musicians[i] + " plays " + instruments[i]);
+  } return emptyArray
 }
 
 function johnLennonFacts(facts){
-  var newArray = []
-  var i = 0
-  while(facts.length>i){
-    newArray.push(facts[i]+"!!!")
-    i++
-  } return newArray
+var emptyArray = [];
+var i = 0;
+while(facts.length>i){
+  emptyArray.push(facts[i]+ "!!!");
+  i++;
+} return emptyArray
 }
 
 function iLoveTheBeatles(n){
-  var newArray=[]
-  do { newArray.push("I love the Beatles!")
-  n++
-} while (n<15)
-return newArray
-}
+  var newArray = [];
 
+  do {newArray.push("I love the Beatles!");
+    n++
+    
+  }
+    while(n<15);
+  return newArray
+}
