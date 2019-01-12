81d0d3e65cfbadb2d3289ce3061f4e943405c8c4
Elizabeth Chan <lchan217@gmail.com>
2019-01-12 14:30:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musician, instruments){
   var newArray = [];
   for(var i = 0; i<musician.length; i++) {
-    newArray.push(`${musician} plays ${instruments)
+    newArray.push(musicians[i] + " plays " + instruments[i])
   }
   return newArray
 }
