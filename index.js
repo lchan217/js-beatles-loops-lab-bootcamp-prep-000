03e67e057386fbe9af29224a7da0c5eeeec723eb
Elizabeth Chan <lchan217@gmail.com>
2019-01-12 14:37:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function johnLennonFacts(facts){
 
 function iLoveTheBeatles(n){
   var newArray=[]
-  do {
-  [loopBody];
-} while ([condition]);
+  do { newArray.push("I love the Beatles!")
+} while (n<15);
+n++
 }
 
